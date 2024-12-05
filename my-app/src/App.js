3fa59e06a56aae8a2a91794532618f1979772b44
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-05 11:24:17 +0000

    duplicated the component file from Stateful Greeting to get ready for this updating the state part 2.

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line 
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback.js';
+
+function App() {
+  return (
+    <div className="App">
+      <StatefulGreetingWithCallback />      
+    </div>
+  );
+}
+
+export default App;
